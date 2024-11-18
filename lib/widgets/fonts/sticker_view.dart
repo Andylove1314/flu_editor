56ec89098cae460cbd74167871e5b5a1896053b5
andy <kun.feng@bigwinepot.com>
2024-11-18 17:06:42 +0800

    fix

--- lib/widgets/fonts/sticker_view.dart
+++ lib/widgets/fonts/sticker_view.dart
@@ -3,12 +3,21 @@ import 'package:lindi_sticker_widget/lindi_controller.dart';
 import 'package:lindi_sticker_widget/lindi_sticker_icon.dart';
 import 'package:lindi_sticker_widget/lindi_sticker_widget.dart';
 
+import '../slider_aloha_parameter.dart';
+
 class StikerView extends StatefulWidget {
+  double stvWidth;
+  double stvHeight;
   Widget bgChild;
 
   Function(LindiController stickerController) onInited;
 
-  StikerView({super.key, required this.bgChild, required this.onInited});
+  StikerView(
+      {super.key,
+      required this.bgChild,
+      required this.stvWidth,
+      required this.stvHeight,
+      required this.onInited});
 
   @override
   State<StatefulWidget> createState() {
@@ -25,37 +34,10 @@ class _StikerViewState extends State<StikerView> {
       borderColor: Colors.white,
       icons: [
         LindiStickerIcon(
-            icon: Icons.done,
-            alignment: Alignment.topRight,
-            onTap: () {
-              _controller.selectedWidget!.done();
-            }),
-        LindiStickerIcon(
-            icon: Icons.lock_open,
-            lockedIcon: Icons.lock,
-            alignment: Alignment.topCenter,
-            type: IconType.lock,
-            onTap: () {
-              _controller.selectedWidget!.lock();
-            }),
-        LindiStickerIcon(
             icon: Icons.close,
             alignment: Alignment.topLeft,
             onTap: () {
               _controller.selectedWidget!.delete();
-            }),
-        LindiStickerIcon(
-            icon: Icons.edit,
-            alignment: Alignment.centerLeft,
-            onTap: () {
-              _controller.selectedWidget!
-                  .edit(const Icon(Icons.star, size: 50, color: Colors.yellow));
-            }),
-        LindiStickerIcon(
-            icon: Icons.layers,
-            alignment: Alignment.centerRight,
-            onTap: () {
-              _controller.selectedWidget!.stack();
             }),
         LindiStickerIcon(
             icon: Icons.flip,
@@ -64,7 +46,7 @@ class _StikerViewState extends State<StikerView> {
               _controller.selectedWidget!.flip();
             }),
         LindiStickerIcon(
-            icon: Icons.crop_free,
+            icon: Icons.cached,
             alignment: Alignment.bottomRight,
             type: IconType.resize),
       ],
@@ -79,9 +61,32 @@ class _StikerViewState extends State<StikerView> {
 
   @override
   Widget build(BuildContext context) {
-    return LindiStickerWidget(
-      controller: _controller,
-      child: widget.bgChild,
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: widget.stvWidth,
+            height: widget.stvHeight,
+            child: LindiStickerWidget(
+              controller: _controller,
+              child: widget.bgChild,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: SliderAlphaParameterWidget(
+            initValue: 1.0,
+            onChanged: (double value) {
+              debugPrint('alpha = $value');
+            },
+          ),
+        )
+      ],
     );
   }
 }
