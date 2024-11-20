c41cc92c43caad1a3ee009cc4303eff8ac843816
andy <kun.feng@bigwinepot.com>
2024-11-20 16:01:02 +0800

    font pan 3

--- lib/widgets/fonts/font_align_pan.dart
+++ lib/widgets/fonts/font_align_pan.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../slider_opacity_parameter_2.dart';
+import 'align/align_widget.dart';
+
 class FontAlginPan extends StatefulWidget {
   /// algin 0 居左 1 居中 2 居右
   final Function({double? worldSpace, double? lineSpace, int algin}) onChanged;
@@ -16,14 +19,39 @@ class _FontAlginPanState extends State<FontAlginPan>
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.green,
+          color: Colors.white,
           boxShadow: [
             BoxShadow(color: const Color(0xff19191A).withOpacity(0.1))
           ],
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(12), topRight: Radius.circular(12))),
       height: 200,
-      child: const SizedBox(),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 33, bottom: 20),
+            child: SliderOpacityParameterWidget2(
+              value: 1.0,
+              onChanged: (double value) {
+                debugPrint('worldspace: $value');
+              },
+            ),
+          ),
+          SliderOpacityParameterWidget2(
+            value: 1.0,
+            onChanged: (double value) {
+              debugPrint('linespace: $value');
+            },
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: AlignWidget(
+              onSelect: (style) {},
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
--- lib/widgets/fonts/font_style_pan.dart
+++ lib/widgets/fonts/font_style_pan.dart
@@ -1,4 +1,9 @@
+import 'package:flu_editor/widgets/fonts/style/style_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../slider_opacity_parameter.dart';
+import '../slider_opacity_parameter_2.dart';
+import 'color/color_widget.dart';
 
 class FontStylePan extends StatefulWidget {
   /// style 0 粗体 1 斜体 2 下划线
@@ -16,14 +21,38 @@ class _FontStylePanState extends State<FontStylePan>
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.red,
+          color: Colors.white,
           boxShadow: [
             BoxShadow(color: const Color(0xff19191A).withOpacity(0.1))
           ],
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(12), topRight: Radius.circular(12))),
       height: 200,
-      child: const SizedBox(),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 33, bottom: 23),
+            child: ColorWidget(
+              onSelect: (color) {
+                debugPrint('color: $color');
+              },
+            ),
+          ),
+          SliderOpacityParameterWidget2(
+            value: 1.0,
+            onChanged: (double value) {
+              debugPrint('opacity: $value');
+            },
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: StyleWidget(
+              onSelect: (style) {},
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
