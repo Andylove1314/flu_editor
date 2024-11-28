1c34936b3da4f36db12789d1c493b99b162622a1
andy <kun.feng@bigwinepot.com>
2024-11-28 19:22:20 +0800

    add local path

--- lib/models/effect_data.dart
+++ lib/models/effect_data.dart
@@ -5,17 +5,23 @@ class EffectData {
   String image;
   String params;
   final id;
+  String? path;
 
   Map<String, dynamic> get paramGroup => jsonDecode(params);
 
   EffectData(
-      {required this.name, required this.image, required this.params, this.id});
+      {required this.name,
+      required this.image,
+      required this.params,
+      this.id,
+      this.path});
 
 // 从 JSON 创建 EffectData 实例
   EffectData.fromJson(Map json)
       : name = json['name'] as String,
         image = json['image'] as String,
         params = json['params'] as String,
+        path = json['path'],
         id = json['id']; // id 可以是任意类型
 
   // 将 EffectData 实例转换为 JSON
@@ -23,6 +29,7 @@ class EffectData {
     return {
       'name': name,
       'image': image,
+      'path': path,
       'params': params,
       'id': id,
     };
